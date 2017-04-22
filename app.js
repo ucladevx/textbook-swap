a4c45478c3462a52e488202bfeb29662b5c2e351
Jayendra <jay08jog@gmail.com>
2017-04-21 22:45:21 -0700

    oops forgot to include these last time

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ const db = require('./models/init.js');
 const initModel = require('./models/init');
 //used to do a quick dirty test of the owned_books interface
 const ownedBooks = require('./models/owned_books');
+const possibleTrades = require('./models/possible_trades');
 /*
  * API keys and Passport configuration.
  */
@@ -48,11 +49,19 @@ ownedBooks.add_book('Adi', 2, test_n);
 ownedBooks.add_book('Adi', 3, test_n);
 ownedBooks.get_owners(2, test_next);
 
+possibleTrades.add_relation('Jay', 10, 20, test_n);
+possibleTrades.add_relation('Jay', 10, 21, test_n);
+possibleTrades.add_relation('Jay', 15, 20, test_n);
+possibleTrades.add_relation('Jay', 1, 12, test_n);
+
 const ec = require('./error_codes.js');
 
 function test_n(errorcode){
     if (errorcode == ec.owned_books_errors.OWNED_BOOK_ALREADY_EXISTS){
         console.log("book is in the database");
+    }
+    if (errorcode == ec.possible_trades_errors.POSSIBLE_TRADE_ALREADY_EXISTS){
+        console.log("relation is in the database");
     }
 }
 
--- error_codes.js
+++ error_codes.js
@@ -8,3 +8,11 @@ exports.owned_books_errors = Object.freeze({
     DB_QUERY_ERROR : 2,
     OWNED_BOOK_ALREADY_EXISTS : 3
 });
+
+
+exports.possible_trades_errors = Object.freeze({
+    DB_SUCCESS : 0,
+    DB_CONNECTION_ERROR : 1,
+    DB_QUERY_ERROR : 2,
+    POSSIBLE_TRADE_ALREADY_EXISTS : 3,
+});

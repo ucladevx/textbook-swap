917f8532f3bb967e8b1208b4370ebf2c54191641
Jayendra <jay08jog@gmail.com>
2017-04-21 22:41:16 -0700

    add_relation works

---
+++ models/possible_trades.js
@@ -0,0 +1,38 @@
+'use strict';
+const pg = require('pg');
+const error_codes = require('../error_codes');
+const conString = 'postgres://Jayendra:@localhost/loopsDB';
+
+exports.add_relation = function(user_id, book_have_id, book_want_id, next){
+    pg.connect(conString, function(err, client, done){
+        done();
+        if (err){
+            console.error("Error connection to client while querying possible_trades table: ", err);
+            return next(error_codes.possible_trades_errors.DB_CONNECTION_ERROR);
+        }
+
+        //check if the relation exists already
+        client.query("SELECT COUNT(user_id) FROM possible_trades WHERE user_id=$1::VARCHAR AND book_have=$2::INTEGER AND book_want=$3::INTEGER", [user_id, book_have_id,book_want_id ], function(err, result){
+            if (err){
+                console.error("Error querying table possible_trades", err);
+                return next(error_codes.possible_trades_errors.DB_QUERY_ERROR);
+            }
+
+            //if relationship doesn't exist, it inserts it, if it does, it returns and error
+            if(result.rows[0].count == 0){
+                client.query("INSERT INTO possible_trades (user_id, book_have, book_want) VALUES ($1::VARCHAR, $2::INTEGER, $3::INTEGER)", [user_id, book_have_id, book_want_id], function(err, result){
+                    if (err){
+                        console.error("Error inserting into possible_trades table", err);
+                        return next(error_codes(error_codes.possible_trades_errors.DB_QUERY_ERROR));
+                    }
+
+                    return next(error_codes.possible_trades_errors.DB_SUCCESS);
+                });
+            }
+            else{
+                console.error("UserID, BookHaveID, BookWantID association already exists in possible_trades table");
+                return next(error_codes.possible_trades_errors.POSSIBLE_TRADE_ALREADY_EXISTS);
+            }
+        });
+    });
+};

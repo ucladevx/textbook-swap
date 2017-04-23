da953b8c916483cc1f402f985a7c9b8ce7d3d474
Jerry Li <jerrylinew@gmail.com>
2017-04-22 18:35:43 -0700

    displays name and email when logged in, and cleaned up auth a bit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const express = require('express'); // express framework for node.js
 const dotenv = require('dotenv'); // loads environment variables
 const path = require('path'); // utilities for working with file and directory paths
 const chalk = require('chalk'); // pretty command line colors
-var passport = require('passport');
-var FacebookStrategy = require('passport-facebook').Strategy;
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
 
 /*
  * TODO: Load environment variables from .env file, where API keys and passwords are configured.
@@ -17,7 +17,6 @@ dotenv.load({ path: '.env' });
  * Controllers (route handlers).
  */
 const homeController = require('./controllers/home');
-// needed for Facebook authentication
 const passportController = require('./controllers/passport');
 
 /*
@@ -36,17 +35,19 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(express.static(path.join(__dirname, 'public')));
-// Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
+
+/*
+ * Use application-level middleware for common functionality, including logging, parsing, and session handling.
+ */
 app.use(require('morgan')('combined'));
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard', resave: false, saveUninitialized: false }));
 
-// Initialize Passport and restore authentication state, if any, from the
-// session.
+/*
+ * Initialize Passport and restore authentication state, if any, from the session.
+ */
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -59,25 +60,30 @@ app.get('/', homeController.index);
 /*
  * API routes.
  */
+
+
+/*
+ * Authentication routes.
+ */
 // Define routes for Facebook authentication
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at '/login/facebook/return'
-app.get('/login/facebook',
-  passport.authenticate('facebook', { scope: ['public_profile', 'email']}));
+app.get('/login/facebook', passport.authenticate('facebook', { scope: ['public_profile', 'email']}));
 
 // Facebook will redirect the user to this URL after approval.  Finish the
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
-app.get('/login/facebook/return', 
-  passport.authenticate('facebook', { failureRedirect: '/login', scope: ['public_profile', 'email'] }), passportController.loginReturn);
+app.get('/login/facebook/return', passport.authenticate('facebook', {
+    failureRedirect: '/login',
+    scope: ['public_profile', 'email']
+}), passportController.loginReturn);
 
 // Log out the user
 app.get('/logout/facebook', passportController.logout);
 
 // implement "profile" view
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(), passportController.profile);
+app.get('/profile', require('connect-ensure-login').ensureLoggedIn(), passportController.profile);
 
 /*
  * Start Express server.
--- controllers/passport.js
+++ controllers/passport.js
@@ -1,14 +1,15 @@
 /*
- * Functions for Facebook Authentication
+ * Routes for Facebook Authentication
  */
 
 // output user profile info to console and redirect user to profile page
 exports.loginReturn = function (req, res) {
     // output the user profile info to console to verify
     // req.user is the authenticated user
+    // TODO: Store user information in database
     console.log(req.user.displayName);  // full name
     console.log(req.user.id);  // user ID
-    // console.log(req.user.emails[0].value);  // email address
+    console.log(req.user.emails[0].value);  // email address
     // successful authentication, redirect so user can see their profile
     res.redirect('/profile');
 };
@@ -25,5 +26,8 @@ exports.logout = function (req, res) {
 
 // redirect user to profile page
 exports.profile = function(req, res) {
-    res.render('profile', { user: req.user });
+    res.render('profile', {
+        name: req.user.displayName,
+        email: req.user.emails[0].value
+    });
 };

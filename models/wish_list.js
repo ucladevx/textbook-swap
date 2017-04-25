b5ec07c84d49a9c884b8597e27b4c613e54748d7
Aditya Raju <rajuaditya@gmail.com>
2017-04-25 16:49:18 -0700

    Added comments to the owned_books model and wish_list model

--- models/wish_list.js
+++ models/wish_list.js
@@ -6,6 +6,11 @@
 const pg = require('pg');
 const error_codes = require('../error_codes');
 
+/*
+ Purpose: This function is used to add a user_id, book_id relation to the wish_list database
+ Inputs: User_id, book_id to be added, and callback function
+ Output: Returns the callback function with a success or error code passed as a parameter
+ */
 exports.add_book = function(user, book, next){
     pg.connect(process.env.DATABASE_URL, function(err, client, done){
         done();
@@ -40,6 +45,11 @@ exports.add_book = function(user, book, next){
     });
 };
 
+/*
+ Purpose: This function is used to remove a user_id, book_id relation from the wish_list database
+ Inputs: User_id, book_id to be removed, and callback function
+ Output: Returns the callback function with a success or error code passed as a parameter
+ */
 exports.remove_book = function(user, book, next){
     pg.connect(process.env.DATABASE_URL, function(err, client, done){
         done();
@@ -58,6 +68,11 @@ exports.remove_book = function(user, book, next){
     });
 };
 
+/*
+ Purpose: This function is used to get all the book_id's that a user_id is related to in the wish_list database
+ Inputs: User_id that you want to know what books he or she wants and callback function
+ Output: Returns the callback function with a success or error code passed as a parameter and a list of the book_ids related to the user
+ */
 exports.get_wanted_books = function(user, next){
     pg.connect(process.env.DATABASE_URL, function(err, client, done){
         done();
@@ -76,6 +91,12 @@ exports.get_wanted_books = function(user, next){
     });
 };
 
+
+/*
+ Purpose: Get the user_ids that own a specific book in the wish_list database
+ Inputs: The book_id that you want to get all the users that own it, and callback function
+ Output: Returns a callback function that has an success or error code passed as a parameter and the resulting list of user_ids as another parameter
+ */
 exports.get_users = function(book, next){
     pg.connect(process.env.DATABASE_URL, function(err, client, done){
         done();

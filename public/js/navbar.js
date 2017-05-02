ca1237fe457a70759bf12dbee1818fe5b88a96f2
Lawrence Chen <lawrencechen98@gmail.com>
2017-05-01 20:03:11 -0700

    Gradient animation with logo

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 
         var scrollPercent = (s / (d-c)) * 100;
         if (scrollPercent > 90) { // check if user scrolled more than 50 from top of the browser window
-            $(".navbar-fixed-top").css("background", "transparent"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+            $(".navbar-fixed-top").css("opacity","0.25"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
         } else {
-            $(".navbar-fixed-top").css("background", "linear-gradient(to right, rgba(255,153,153,1),rgba(255,204,153,1)"); // if not, change it back to transparent
+            $(".navbar-fixed-top").css("opacity", "1.0"); // if not, change it back to transparent
         }
     });
 });

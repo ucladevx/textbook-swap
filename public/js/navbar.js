82b5d80df898fc60c5a6abe92ac9dac6f879c0ea
Lawrence Chen <lawrencechen98@gmail.com>
2017-05-02 17:27:08 -0700

    Fixed svg font, navbar animation

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
     var scrollPercent = (s / (d-c)) * 100;
     if (scrollPercent > 97) { // check if user scrolled more than 50 from top of the browser window
-        $(".navbar-fixed-top").css("opacity","0.7");
+        $(".navbar-fixed-top").css("opacity","0.6");
         $("nav").css('box-shadow', '0px 1px 8px #555');
     } else {
         $(".navbar-fixed-top").css("opacity", "1.0");
@@ -18,18 +18,18 @@ $(document).ready(function(){
 
         var scrollPercent = (s / (d-c)) * 100;
         if (scrollPercent > 97) { // check if user scrolled more than 50 from top of the browser window
-            $(".navbar-fixed-top").css("opacity","0.7");
+            $(".navbar-fixed-top").css("opacity","0.6");
             $("nav").css('box-shadow', '0px 1px 8px #555');
+            $("nav").hover(function(){
+                $(".navbar-fixed-top").css("opacity", "1.0");
+                $("nav").css('box-shadow', '0px 1px 20px #555');
+            },function(){
+                $(".navbar-fixed-top").css("opacity","0.6");
+                $("nav").css('box-shadow', '0px 1px 8px #555');
+            });
         } else {
             $(".navbar-fixed-top").css("opacity", "1.0");
             $("nav").css('box-shadow', '0px 1px 20px #555');
         }
-    });
-    $("nav").hover(function(){
-        $(".navbar-fixed-top").css("opacity", "1.0");
-        $("nav").css('box-shadow', '0px 1px 20px #555');
-    },function(){
-        $(".navbar-fixed-top").css("opacity","0.7");
-        $("nav").css('box-shadow', '0px 1px 8px #555');
     });
 });

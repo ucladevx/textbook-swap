422027015ef10774ed8d65300d2e2227ca4312c5
Lawrence Chen <lawrencechen98@gmail.com>
2017-05-02 17:45:48 -0700

    Merged top and bottom of landing page, fixed bugs

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -7,9 +7,23 @@ $(document).ready(function(){
     if (scrollPercent > 97) { // check if user scrolled more than 50 from top of the browser window
         $(".navbar-fixed-top").css("opacity","0.6");
         $("nav").css('box-shadow', '0px 1px 8px #555');
+        $("nav").hover(function(){
+            $(".navbar-fixed-top").css("opacity", "1.0");
+            $("nav").css('box-shadow', '0px 1px 20px #555');
+        },function(){
+            $(".navbar-fixed-top").css("opacity","0.6");
+            $("nav").css('box-shadow', '0px 1px 8px #555');
+        });
     } else {
         $(".navbar-fixed-top").css("opacity", "1.0");
         $("nav").css('box-shadow', '0px 1px 20px #555');
+        $("nav").hover(function(){
+            $(".navbar-fixed-top").css("opacity", "1.0");
+            $("nav").css('box-shadow', '0px 1px 20px #555');
+        },function(){
+            $(".navbar-fixed-top").css("opacity", "1.0");
+            $("nav").css('box-shadow', '0px 1px 20px #555');
+        });
     }
     $(window).scroll(function() { // check if scroll event happened
         var s = $(window).scrollTop(),
@@ -17,7 +31,7 @@ $(document).ready(function(){
             c = $(window).height();
 
         var scrollPercent = (s / (d-c)) * 100;
-        if (scrollPercent > 97) { // check if user scrolled more than 50 from top of the browser window
+        if (scrollPercent > 97) {
             $(".navbar-fixed-top").css("opacity","0.6");
             $("nav").css('box-shadow', '0px 1px 8px #555');
             $("nav").hover(function(){
@@ -30,6 +44,13 @@ $(document).ready(function(){
         } else {
             $(".navbar-fixed-top").css("opacity", "1.0");
             $("nav").css('box-shadow', '0px 1px 20px #555');
+            $("nav").hover(function(){
+                $(".navbar-fixed-top").css("opacity", "1.0");
+                $("nav").css('box-shadow', '0px 1px 20px #555');
+            },function(){
+                $(".navbar-fixed-top").css("opacity", "1.0");
+                $("nav").css('box-shadow', '0px 1px 20px #555');
+            });
         }
     });
 });

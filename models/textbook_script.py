dfd56e655b1acb49036cebbe2d49a8e815219ec0
Jayendra <jay08jog@gmail.com>
2017-05-04 00:06:59 -0700

    multiple books for a class support:

--- models/textbook_script.py
+++ models/textbook_script.py
@@ -7,27 +7,45 @@ wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 
 final = set()
 for i in range(100000, 200000):
+	print i
 	url = "http://ucla.verbacompare.com/comparison?id=" + str(i)
-
 	r  = requests.get(url)
-
 	data = r.text
-
 	soup = BeautifulSoup(data, "html.parser")
-	elements = [];
+	elements = {};
 	for script in soup.find_all('script'):
 		if 'Verba.Compare.Collections.Sections' in str(script):
+			index = "0"
 			for word in str(script).split(','):
-				if "title" == word.split(":")[0].strip('"') and word != '"title":null':
-					elements.append(word.split(":")[1].strip('"').decode("unicode-escape"))
-				elif 'author' == word.split(":")[0].strip('"'):
-					elements.append(word.split(":")[1].strip('"').decode("unicode-escape"))
-				elif 'edition' == word.split(":")[0].strip('"') and word.split(":")[1].strip('"').isdigit():
-					elements.append(word.split(":")[1].strip('"').decode("unicode-escape"))
+				#instructor always comes first
+				if "instructor" == word.split(":")[0].strip('"'):
+					elements['instructor'] = word.split(":")[1].strip('"').decode("unicode-escape")
+				#isbn number comes next, but doesnt always exist
 				elif 'isbn' == word.split(":")[0].strip('"'):
-					elements.append(word.split(":")[1].strip('"').decode("unicode-escape"))
-		
-	if len(elements) == 5 and str(elements) not in final:
-		elements.append(str(i))
-		wr.writerow(elements)
-		final.add(str(elements))
+					elements['isbn' + index] = word.split(":")[1].strip('"').decode("unicode-escape")
+				#cover image comes next
+				elif 'cover_image_url' == word.split(":")[0].strip('"'):
+					elements['image' + index] = "https:" + word.split(":")[1].strip('"')
+				#title is always guaranteed to occur for a book
+				elif "title" == word.split(":")[0].strip('"') and word != '"title":null':
+					#very first thing is title, corresponding to course title
+					if index == "0":
+						elements["class"] = word.split(":")[1].strip('"').decode("unicode-escape")
+						index = "1"
+					#if isbn number was not found before
+					if not 'isbn' + index in elements:
+						elements['isbn' + index] = ""
+					#if image wasn't found before
+					if not 'image' + index in elements:
+						elements['image' + index] = ""
+					elements['title' + index] = word.split(":")[1].strip('"').decode("unicode-escape")
+				elif 'author' == word.split(":")[0].strip('"'):
+					elements['author' + index] = word.split(":")[1].strip('"').decode("unicode-escape")
+					#once the author field is hit, we've gotten all the data we need for this book
+					index = str(int(index) + 1)
+
+	if 'instructor' in elements and 'title1' in elements: 
+		for j in range(1, int(index)):
+			temp = [elements['instructor'], elements['class'],  elements['title' + str(j)], elements['author' + str(j)], elements['image' + str(j)], elements['isbn' + str(j)], str(i)]
+			wr.writerow(temp)
+			final.add(str(elements))

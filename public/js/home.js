59fdd9bb430128b0ff4ee9c2b3e5322921e28f0f
Lawrence Chen <lawrencechen98@gmail.com>
2017-05-09 14:16:25 -0700

    Everything done but animation

--- public/js/home.js
+++ public/js/home.js
@@ -21,3 +21,28 @@ $("#navbar_logo").click(function(){
         scrollTop: "0px"
     }, 500);
 });
+
+
+
+$(document).ready(function(){
+
+    var index = 0;
+    var images=[];
+
+    for(var i = 1; i <= 143; i+=1){
+        var string = "/images/loop_animation/loopgraphic_00" + ("000"+i).slice(-3) + ".png";
+        images[i-1] = new Image();
+        images[i-1].src = string;
+    }
+
+    function loop_animate(){
+        $("#second").css('background-image', images[index]);
+        index += 1;
+    }
+
+    loop_animate();
+
+    window.setInterval(loop_animate, 100);
+
+
+});

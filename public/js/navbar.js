1af75a4641949f6dd679aa69f41aa56ec0278583
Lawrence Chen <lawrencechen98@gmail.com>
2017-05-16 17:21:06 -0700

    Removed navbar shadow

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -9,20 +9,15 @@ $(document).ready(function(){
         $("nav").css('box-shadow', '0px 1px 8px #555');
         $("nav").hover(function(){
             $(".navbar-fixed-top").css("opacity", "1.0");
-            $("nav").css('box-shadow', '0px 1px 20px #555');
         },function(){
             $(".navbar-fixed-top").css("opacity","0.6");
-            $("nav").css('box-shadow', '0px 1px 8px #555');
         });
     } else {
         $(".navbar-fixed-top").css("opacity", "1.0");
-        $("nav").css('box-shadow', '0px 1px 20px #555');
         $("nav").hover(function(){
             $(".navbar-fixed-top").css("opacity", "1.0");
-            $("nav").css('box-shadow', '0px 1px 20px #555');
         },function(){
             $(".navbar-fixed-top").css("opacity", "1.0");
-            $("nav").css('box-shadow', '0px 1px 20px #555');
         });
     }
     $(window).scroll(function() { // check if scroll event happened
@@ -33,23 +28,17 @@ $(document).ready(function(){
         var scrollPercent = (s / (d-c)) * 100;
         if (scrollPercent > 97) {
             $(".navbar-fixed-top").css("opacity","0.6");
-            $("nav").css('box-shadow', '0px 1px 8px #555');
             $("nav").hover(function(){
                 $(".navbar-fixed-top").css("opacity", "1.0");
-                $("nav").css('box-shadow', '0px 1px 20px #555');
             },function(){
                 $(".navbar-fixed-top").css("opacity","0.6");
-                $("nav").css('box-shadow', '0px 1px 8px #555');
             });
         } else {
             $(".navbar-fixed-top").css("opacity", "1.0");
-            $("nav").css('box-shadow', '0px 1px 20px #555');
             $("nav").hover(function(){
                 $(".navbar-fixed-top").css("opacity", "1.0");
-                $("nav").css('box-shadow', '0px 1px 20px #555');
             },function(){
                 $(".navbar-fixed-top").css("opacity", "1.0");
-                $("nav").css('box-shadow', '0px 1px 20px #555');
             });
         }
     });

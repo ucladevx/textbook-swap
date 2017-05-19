1aa56675f589e3edf015769c46d2deacce75997d
Jerry Li <jerrylinew@gmail.com>
2017-05-19 10:05:17 -0700

    loaded owned books data from backend

--- controllers/routes/bookshelf.js
+++ controllers/routes/bookshelf.js
@@ -3,6 +3,31 @@
  * Bookshelf.
  */
 
+const owned_books = require('../../models/owned_books');
+const book_info = require('../../models/book_info');
+const error_codes = require('../../error_codes');
+
 exports.index = function(req, res) {
-    res.render('bookshelf');
+    var user_id = req.user.id;
+
+    owned_books.get_owned_books(user_id, function(status, data){
+        if (status == error_codes.owned_books_errors.DB_SUCCESS)
+            console.log("Successfully found books from the database!");
+
+        // convert from array of Javascript objects to just array of book_ids
+        var book_ids = [];
+        for (var i = 0; i < data.length; i++) {
+            book_ids.push(data[i]["book_id"]);
+        }
+
+        // get the book info corresponding to each book_id
+        book_info.get_books_info(book_ids, function(error_status, books_data) {
+            if (error_status) {
+                console.error("Error querying database", error_status);
+            }
+            // return the info for all the books
+            console.log(books_data);
+            res.render('bookshelf', {status: error_status, books: books_data});
+        });
+    });
 };

1a3c7d5168c0dbca066a73f8a08d6ddc4976d4f4
Jayendra <jay08jog@gmail.com>
2017-05-26 20:54:20 -0700

    basic filtering works

--- public/js/bookshelf.js
+++ public/js/bookshelf.js
@@ -1,31 +1,11 @@
-// init Isotope
-
-// $(document).ready(function(){
-// 	var elem = document.querySelector('.grid');
-// 	var iso = new Isotope( elem, {
-// 	  // options
-// 	  itemSelector: '.grid-item',
-// 	});
-
-// });
-
 $(document).ready(function(){
-	$(".body").click(function(e) {
-		console.log("grid click");
+	var $grid = $('.grid').isotope({
+   itemSelector: '.grid-item',
 	});
 
-	$(".filter-button").on('click', function () {
-		console.log("click recognized");
-		// var filterValue = button.getAttribute('data-filter');
-		// iso.arrange({ filter: filterValue });
+	// filter items on button click
+	$('.filter-button-group').on( 'click', 'button', function() {
+	  var filterValue = $(this).attr('data-filter');
+	  $grid.isotope({ filter: filterValue });
 	});
 });
-
-// // filter items on button click
-// $('.filter-button-group').on( 'click', 'button', function() {
-//   var filterValue = $(this).attr('data-filter');
-
-//   console.log(filterValue);
-
-//   //$grid.isotope({ filter: filterValue });
-// });

142b5df6936173a1a92f2d1d2a9ab6c9808f0635
Jayendra <jay08jog@gmail.com>
2017-05-26 18:47:36 -0700

    broken button code

---
+++ public/js/bookshelf.js
@@ -0,0 +1,29 @@
+// init Isotope
+
+// $(document).ready(function(){
+// 	var elem = document.querySelector('.grid');
+// 	var iso = new Isotope( elem, {
+// 	  // options
+// 	  itemSelector: '.grid-item',
+// 	});
+
+// });
+
+$(".body").click(function(e) {
+	console.log("grid click");
+});
+
+$(".filter-button").click(function () {
+		console.log("click recognized");
+		// var filterValue = button.getAttribute('data-filter');
+		// iso.arrange({ filter: filterValue });
+	});
+
+// // filter items on button click
+// $('.filter-button-group').on( 'click', 'button', function() {
+//   var filterValue = $(this).attr('data-filter');
+
+//   console.log(filterValue);
+
+//   //$grid.isotope({ filter: filterValue });
+// });

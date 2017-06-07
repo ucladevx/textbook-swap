dc35a35c13a55ed48d0bc8b57cf5a8e0d2cb1dd1
Jayendra <jay08jog@gmail.com>
2017-06-06 21:26:01 -0700

    get trades status

--- models/found_trades.js
+++ models/found_trades.js
@@ -1,4 +1,4 @@
-/*
+ /*
  * Interface to query and modify the table found_trades.
  */
 
@@ -312,3 +312,41 @@ exports.get_matched_trades = function (user_id, next){
             });
     });
 };
+
+exports.get_trades_status = function (trade_id, next) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done){
+        done();
+
+        if (err){
+            console.error("Error connection to client while querying found_trades table: ", err);
+            return next(error_codes.found_trades_errors.DB_CONNECTION_ERROR, []);
+        }
+        var rejected = false;
+        var accepted = true;
+        var result;
+        get_statuses_by_id(trade_id, function(error_code, statuses) {
+            (statuses).forEach(function(row, i_index) {
+               console.log(row);
+               switch(row) {
+                case 'A':
+                    break;
+                case 'P':
+                    accepted = false;
+                    break;
+                case 'R':
+                    rejected = true;
+                    accepted = false;
+                    break;
+                default:
+                    break;
+               }
+               if(i_index == statuses.length - 1) {
+                if(rejected) result = 'R';
+                else if(accepted) result = 'A';
+                else result = 'P';
+                return next(error_codes.found_trades_errors.DB_SUCCESS, result);
+               }
+            }
+        });
+    }
+}

adfce3bd9b2e34712f3913b5e57ddae1d1bfe798
Karen Li <karenli.614@gmail.com>
2018-03-14 16:48:06 -0700

    Fix social media logo colours

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -10,8 +10,8 @@ const Footer = () => {
     return (
         <footer id="myFooter">
         <div className="social-networks">
-            <a href="#" className="twitter"><Facebook/></a>
-            <a href="#" className="facebook"><Twitter></Twitter></a>
+            <a href="#" className="facebook"><Facebook/></a>
+            <a href="#" className="twitter"><Twitter></Twitter></a>
             <a href="#" className="google"><Google></Google></a>
             <p>© 2017 Loop UCLA DevX </p>
         </div>
